727c35dde0dcd28836aa3f17fe9ffb0ad011038d
panditdhamdhere <dhamdherepandit@gmail.com>
2023-05-19 14:44:20 +0530

    adding navbar

--- src/App.js
+++ src/App.js
@@ -6,26 +6,101 @@ import {
   queryExplorePublications,
   queryRecommendedProfiles,
 } from "./queries";
-import LENSHUB from './lenshub.json'
-import { ethers } from 'ethers'
-import { Box, Button, Image} from '@chakra-ui/react';
+import LENSHUB from "./lenshub.json";
+import { ethers } from "ethers";
+import { Box, Button, Image } from "@chakra-ui/react";
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [profiles, setProfiles] = useState([]);
+  console.log("~profiles", profiles);
+  const [posts, setPosts] = useState([]);
+  console.log("~posts", posts);
 
-const [account, setAccount] = useState(null);
-const [profiles, setProfiles] = useState([]);
-const [posts, setPosts] = useState([])
+  async function signIn() {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
+  }
 
+  async function getRecommendedProfiles() {
+    const response = await urlClient
+      .query(queryRecommendedProfiles)
+      .toPromise();
 
-async function signIn() {
-  
-}
+    const profiles = response.data.recommendedProfiles.slice(0, 5);
+    setProfiles(profiles);
+  }
 
-  return(
-   <div className="app">
+  async function getPosts() {
+    const response = await urlClient
+      .query(queryExplorePublications)
+      .toPromise();
+    const posts = response.data.explorePublications.items.filter((post) => {
+      if (post.profile) return post;
+      return "";
+    });
+    setPosts(posts);
+  }
 
-  </div>
-  )
+  async function follow(id) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(
+      LENS_HUB_CONTRACT_ADDRESS,
+      LENSHUB,
+      provider.getSigner()
+    );
+    const tx = await contract.follow([parseInt(id)], [0x0]);
+    await tx.wait();
+  }
+
+  useEffect(() => {
+    getRecommendedProfiles();
+    getPosts();
+  }, []);
+
+  return (
+    <div className="app">
+      <Box width="100%" backgroundColor="rgba(5, 32, 64, 28)">
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          width="55%"
+          margin="auto"
+          color="white"
+          padding="10px 0"
+        >
+          <Box>
+            <Box
+              fontFamily="DM Serif Display"
+              fontSize="44px"
+              fontStyle="italic"
+            >
+              D Centra
+            </Box>
+            <Box>Decentralized Social Media App</Box>
+          </Box>
+          {account ? (
+            <Box backgroundColor="#000" padding="15px" borderRadius="6px">
+              Connected
+            </Box>
+          ) : (
+            <Button
+              onClick={signIn}
+              color="rgba(5,32,64)"
+              _hover={{ backgroundColor: "#808080" }}
+            >
+              Connect
+            </Button>
+          )}
+        </Box>
+      </Box>
+
+      {/* content */}
+    </div>
+  );
 }
 
 export default App;
